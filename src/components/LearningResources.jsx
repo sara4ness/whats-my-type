44da971752de44a296ae81114f2e8ad3b1eff879
sara4ness <saraholteness@gmail.com>
2026-01-14 23:27:41 +0000

    Revert "summary button"
    This reverts commit 3fa6ca090f59f1ce570094dc70ee2264d51522f1.

--- src/components/LearningResources.jsx
+++ src/components/LearningResources.jsx
@@ -145,7 +145,6 @@ function LearningResources({ fontFamily, fontSize, lineHeight, textColor, bgColo
       <Navbar 
         onStartOver={onClose} 
         onSkipToLearning={() => {}} 
-        onViewSummary={handleViewSummary}
         showLearningButton={false} 
         fontFamily={fontFamily}
         fontSize={fontSize}
